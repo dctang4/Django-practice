99d4ee964d48408e28e587fecb29ca7e307c186f
Chun Hin D. Tang <dtang955472@gmail.com>
2021-07-13 17:24:49 -0400

    added a html template

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<h1>Hey, Welcome</h1>')
+  # name = 'Jack'
+  context = {
+    'name': 'Patrick',
+    'age': 23,
+    'nationality': 'British'
+  }
+  return render(request, 'index.html', context)
+
+
+def counter(request):
+  text = request.POST['text']
+  amount_of_words = len(text.split())
+  return render(request, 'counter.html', {'amount': amount_of_words})
