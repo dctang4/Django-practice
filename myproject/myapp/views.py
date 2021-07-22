66a613c0b0e84e0529c7b350912c5f4ea351db80
Chun Hin D. Tang <dtang955472@gmail.com>
2021-07-22 17:29:40 -0400

    adding authentication

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import Feature
 
 # Create your views here.
 def index(request):
+  features = Feature.objects.all()
+  return render(request, 'index.html', {'features': features})
+
   # name = 'Jack'
   # context = {
   #   'name': 'Patrick',
@@ -46,11 +51,32 @@ def index(request):
 
   # return render(request, 'index.html', {'feature1': feature1, 'feature2': feature2, 'feature3': feature3, 'feature4': feature4})
 
-  features = Feature.objects.all()
-  return render(request, 'index.html', {'features': features})
-
-
 def counter(request):
   text = request.POST['text']
   amount_of_words = len(text.split())
   return render(request, 'counter.html', {'amount': amount_of_words})
+
+def register(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    if password == password2:
+      if User.objects.filter(email=email).exists():
+        messages.info(request, 'Email Already Used')
+        return redirect('register')
+      elif User.objects.filter(username=username).exists():
+        messages.info(request, 'Username Already Used')
+        return redirect('register')
+      else:
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return redirect('login')
+    else: 
+      messages.info(request, 'Password Not the Same')
+      return redirect('register')
+
+  else:
+    return render(request, 'register.html')
